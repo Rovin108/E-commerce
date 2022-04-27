2958b7729630e2190e742d046f9f05a395768b85
unknown <=>
2022-04-27 15:36:21 +0530

    second

---
+++ ecomapp/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from ecomapp.views import *
+
+app_name = "ecomapp"
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('contact-us/', ContactView.as_view(), name='contact'),
+    path('all-products/',AllProductsView.as_view(), name="allproducts"),
+    path("product/<slug:slug>", ProductDetailView.as_view(), name="productdetail"),
+    path("add-to-cart-<int:pro_id>/", AddToCartView.as_view(), name="addtocart"),
+    path("my-cart/", MyCartView.as_view(), name="mycart"),
+    path("manage-cart/<int:cp_id>/", ManageCartView.as_view(), name="managecart"),
+    path("empty-cart/", EmptyCartView.as_view(), name="emptycart"),
+    path("checkout/", CheckoutView.as_view(), name="checkout"),
+    path("register/", CustomerRegistrationView.as_view(), name="customerregistration"),
+    path("logout/", CustomerLogoutView.as_view(), name="customerlogout"),
+    path("login/", CustomerLoginView.as_view(), name="customerlogin"),
+]
